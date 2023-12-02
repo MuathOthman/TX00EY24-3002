f1a07cb84252b0aca0f8808fa32d4e85bbad0de6
Muath Othman <muath.othman@gmail.com>
2023-12-02 20:13:54 +0200

    Needed to do new branch

---
+++ Frontend_1/src/Dashboard/Charts/data.js
@@ -0,0 +1,110 @@
+const data = [
+    {
+        id: 'japan',
+        color: 'hsl(169, 70%, 50%)',
+        data: [
+            {
+                x: 'plane',
+                y: 274,
+            },
+            {
+                x: 'helicopter',
+                y: 63,
+            },
+            {
+                x: 'boat',
+                y: 13,
+            },
+            {
+                x: 'train',
+                y: 98,
+            },
+            {
+                x: 'subway',
+                y: 56,
+            },
+            {
+                x: 'bus',
+                y: 84,
+            },
+            {
+                x: 'car',
+                y: 48,
+            },
+            {
+                x: 'moped',
+                y: 69,
+            },
+            {
+                x: 'bicycle',
+                y: 80,
+            },
+            {
+                x: 'horse',
+                y: 4,
+            },
+            {
+                x: 'skateboard',
+                y: 109,
+            },
+            {
+                x: 'others',
+                y: 47,
+            },
+        ],
+    },
+    {
+        id: 'france',
+        color: 'hsl(48, 70%, 50%)',
+        data: [
+            {
+                x: 'plane',
+                y: 165,
+            },
+            {
+                x: 'helicopter',
+                y: 43,
+            },
+            {
+                x: 'boat',
+                y: 8,
+            },
+            {
+                x: 'train',
+                y: 50,
+            },
+            {
+                x: 'subway',
+                y: 24,
+            },
+            {
+                x: 'bus',
+                y: 49,
+            },
+            {
+                x: 'car',
+                y: 19,
+            },
+            {
+                x: 'moped',
+                y: 36,
+            },
+            {
+                x: 'bicycle',
+                y: 38,
+            },
+            {
+                x: 'horse',
+                y: 2,
+            },
+            {
+                x: 'skateboard',
+                y: 52,
+            },
+            {
+                x: 'others',
+                y: 26,
+            },
+        ],
+    },
+];
